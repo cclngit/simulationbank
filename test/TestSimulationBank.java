d657b0e4f3ef634e9b415aea59a9fe70b0a8cb16
cclngit <38783129+cclngit@users.noreply.github.com>
2021-12-29 03:52:00 +0100

    v1

--- test/TestSimulationBank.java
+++ test/TestSimulationBank.java
@@ -1,11 +1,11 @@
 package test;
 //import banque.*;
+
 //import simulation.*;
 
-public class TestSimulationBank{
-    public static void main(String[] args){
-    
-       
+public class TestSimulationBank {
+    public static void main(String[] args) {
+
     }
 
 }
