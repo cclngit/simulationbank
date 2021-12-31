e057588bf4e69546eb8415cc002fbe11ce40087d
cclngit <38783129+cclngit@users.noreply.github.com>
2021-12-31 01:52:10 +0100

    prototipe class Caissier

--- banque/Caissier.java
+++ banque/Caissier.java
@@ -1,5 +1,43 @@
 package banque;
 
 public class Caissier {
-    
+    // protected int nbCaissiers; // nombre de caissiers // C EST LA BANQUE QUI CONNAIT LE NB DE CAISSIERS
+    protected Client c; // le Client du Caissier
+    protected float meanTimeCaissiers; // temps moyent de traitement du caissier
+    protected float meanTimeArrival; // temps moyen d'arriver de Clients
+
+    public Caissier(float mTC, float mTA) {
+        // nbCaissiers = nbC;
+        c = new Client();
+        meanTimeCaissiers = mTC;
+        meanTimeArrival = mTA;
+    }
+
+    public float tempsMoyenService() {
+
+        return 0;
+    }
+
+    public int nbClients() {
+
+        return 0;
+    }
+
+    public float tauxOccupation() {
+
+        return 0;
+    }
+
+    public boolean estLibre() {
+
+        return true;
+    }
+
+    public void servic(Client c) {
+
+    }
+
+    public void attendre() {
+
+    }
 }
