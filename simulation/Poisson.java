4d4f08509b3d99cd03dc75293bb3396b0e944e11
cclngit <38783129+cclngit@users.noreply.github.com>
2022-01-04 10:20:44 +0100

    Poinsson

--- simulation/Poisson.java
+++ simulation/Poisson.java
@@ -5,8 +5,9 @@ package simulation;
 import java.util.*;
 
 /**
- * Classe permettant de générer des nombres aléatoires suivant une loi 
+ * Classe permettant de générer des nombres aléatoires suivant une loi
  * de Poisson
+ * 
  * @author laroque@u-cergy.fr
  * @version $Rev: 1905 $
  */
@@ -19,43 +20,49 @@ public class Poisson {
   //////////////////////////////////////////////////////////////
   /**
    * obtient le prochain nombre aléatoire
+   * 
    * @param m la valeur moyenne souhaitée
    * @return un nombre réel positif
    */
   public static double next(double m) {
     return -m * Math.log(r.nextDouble());
   }
-    
 
   //////////////////////////////////////////////////////////////
   /**
    * obtient le prochain nombre aléatoire (centré sur 1)
+   * 
    * @return un nombre réel positif
    */
-  public static double next() { return next(1.0); }
-  
-  /** test du générateur. Paramètres: <ul>
-   * <li> N le nombre de valeurs à générer
-   * <li> m la valeur moyenne souhaitée </ul>
+  public static double next() {
+    return next(1.0);
+  }
+
+  /**
+   * test du générateur. Paramètres:
+   * <ul>
+   * <li>N le nombre de valeurs à générer
+   * <li>m la valeur moyenne souhaitée
+   * </ul>
    */
   public static void main(String args[]) {
-    int N=1000;
+    int N = 1000;
     double moy = 1;
-    //pl.base.Help.usage("pl.simulation.Poisson",args,"<nbIter> <avgValue>");
+    // pl.base.Help.usage("pl.simulation.Poisson",args,"<nbIter> <avgValue>");
     try {
       N = Integer.parseInt(args[0]);
       moy = Double.parseDouble(args[1]);
-    } catch (Exception e) {}
-
+    } catch (Exception e) {
+    }
 
     System.err.println("Running " + N + " iterations on average value "
-                       + moy);
+        + moy);
 
     double res = 0;
 
-    for (int i = 0; i < N; i++) 
+    for (int i = 0; i < N; i++)
       res += Poisson.next(moy);
 
-    System.out.println(res/N);
+    System.out.println(res / N);
   }
 }
