d62fae9d46f8bd0ae0428dbdea5a897ca62c9368
cclngit <38783129+cclngit@users.noreply.github.com>
2022-01-07 23:41:24 +0100

    --

--- banque/Caissier.java
+++ banque/Caissier.java
@@ -3,8 +3,6 @@ package banque;
 import java.util.Scanner;
 
 public class Caissier {
-    // protected int nbCaissiers; // nombre de caissiers // C EST LA BANQUE QUI
-    // CONNAIT LE NB DE CAISSIERS
     protected Client c; // le Client du Caissier
     protected float tempsMoyTraitement; // temps moyent de traitement du caissier
     protected float tauxOcuppattion;
