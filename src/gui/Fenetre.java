89d84e153889f2a8caf7a81bae7f372f716a6991
Dieunelson Dorcelus <43044653+dd701116@users.noreply.github.com>
2022-01-09 18:22:01 +0100

    ajout interface

---
+++ src/gui/Fenetre.java
@@ -0,0 +1,50 @@
+package gui;
+
+import banque.SimulationBanque;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Fenetre implements ActionListener {
+    private JPanel panel1;
+    private JTextField dureeSimulation;
+    private JTextField nombreCaissier;
+    private JTextField dureeMoyenService;
+    private JTextField dureeEntreArriveeClient;
+    private JTextPane resultatSimulation;
+    private JButton lancerButton;
+    private SimulationBanque simulationBanque;
+
+    public Fenetre() {
+        lancerButton.addActionListener(this);
+    }
+
+    public void start() {
+        JFrame frame = new JFrame("BANQUE SIMULATION");
+        frame.setContentPane(this.panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        new Fenetre().start();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this.lancerButton){
+            double dureeSimulation; int nombreCaissier; double dureeMoyenService; double dureeEntreArriveeClient;
+            dureeSimulation = Double.parseDouble(this.dureeSimulation.getText());
+            nombreCaissier = Integer.parseInt(this.nombreCaissier.getText());
+            dureeMoyenService = Double.parseDouble(this.dureeMoyenService.getText());
+            dureeEntreArriveeClient = Double.parseDouble(this.dureeEntreArriveeClient.getText());
+            this.simulationBanque = new SimulationBanque(dureeSimulation, nombreCaissier, dureeMoyenService, dureeEntreArriveeClient);
+            this.simulationBanque.initialiser();
+            this.simulationBanque.executer();
+            this.resultatSimulation.setText(this.simulationBanque.resultat());
+        }
+    }
+}
